a4851e57d3c184840a0edaa39af66309359bcb6c
Cristian T <Cristian.gorjeanul@gmail.com>
2020-03-19 11:20:00 +0200

    Change score color

--- js/app.js
+++ js/app.js
@@ -2,9 +2,11 @@ document.addEventListener('DOMContentLoaded', function () {
 
     var button = document.querySelector('#button');
     var counter = 1;
-
+    
     button.addEventListener('click', function () {
         document.querySelector('p span').innerText = counter;
+        document.querySelector('p span').style.color = "#ff0000"
+        document.querySelector('p span').style.backgroundColor = "#00ff7f"
         counter++;
     });
 
